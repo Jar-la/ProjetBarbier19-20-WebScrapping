04ef99e1b5950e0701ef2fb76de8155cf834a221
Alexa <Alexa@LAPTOP-ALafon>
2019-12-10 03:09:25 +0100

    Début de présantation du site

--- WebSockets_Tyrus_1_15.Java.Maven/web/index.js
+++ WebSockets_Tyrus_1_15.Java.Maven/web/index.js
@@ -1,6 +1,15 @@
 "use strict";
 
-window.onload = () => {};
+window.onload = () => {
+  // Changer l'affichage du nombre d'article en fonction du slidder.
+  let sliderArticles = document.getElementById("rangeNbArticle");
+  let outputArticles = document.getElementById("nbArticles");
+  outputArticles.innerHTML = sliderArticles.value;
+
+  sliderArticles.oninput = function() {
+    outputArticles.innerHTML = this.value;
+  };
+};
 
 // Tested with Tyrus 1.15 WebSockets Java library
 let service = new WebSocket("ws://localhost:1963/FranckBarbier/WebSockets_illustration");
@@ -32,7 +41,9 @@ service.onerror = () => {
   window.alert("service.onerror...");
 };
 
+// Création de la requette pour les articles
 function request() {
+  console.log(document.getElementById("s").value);
   let search = document.getElementById("s").value;
   let searchReg = search.replace(/[^a-z0-9]/gi, "");
   let quantity = 1;
