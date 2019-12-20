ecc8538034daadf9601103a151f1feb1b29bac69
Alexa <Alexa@LAPTOP-ALafon>
2019-12-20 18:25:37 +0100

    crêpes

--- WebSockets_Tyrus_1_15.Java.Maven/web/index.js
+++ WebSockets_Tyrus_1_15.Java.Maven/web/index.js
@@ -19,7 +19,7 @@ window.onload = () => {
   service.onclose = (event /*:CloseEvent*/) => {
     console.log("service.onclose... " + event.code);
     window.alert("Bye! See you later...");
-    window.close()
+    window.close();
     // '1011': the server is terminating the connection because it encountered an unexpected condition that prevented it from fulfilling the request.
   };
   service.onerror = () => {
@@ -207,7 +207,7 @@ window.onload = () => {
     let search = document.getElementById("s").value;
 
     // On enleve tous ce qui n'est pas un chiffre ou une lettre
-    let searchReg = search.replace(/[^a-z0-9\s]/gi, "");
+    let searchReg = search.replace(/[^a-z0-9éèàêù\s]/gi, "");
 
     // Envoie la saisie de l'utilisateur fitré et la quantité si la chaine est non vide et
     // si la recherche ou la quantité à changée
@@ -215,8 +215,8 @@ window.onload = () => {
       oldRequest = searchReg;
       oldQuantity = quantity;
       let data = service.send(
-      JSON.stringify({ Request: `${searchReg}`, Quantity: `${quantity}` }));
+        JSON.stringify({ Request: `${searchReg}`, Quantity: `${quantity}` })
+      );
     }
   };
 };
-
